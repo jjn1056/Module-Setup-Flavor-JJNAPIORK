c1ad16786c892a53387d8a0e88850c343137e791
John Napiorkowski <jjn1056@yahoo.com>
2010-09-17 11:59:48 -0400

    created namespace

--- lib/Module/Setup/Flavor/JJNAPIORK.pm
+++ lib/Module/Setup/Flavor/JJNAPIORK.pm
@@ -1,32 +1,211 @@
 package Module::Setup::Flavor::JJNAPIORK;
+use base 'Module::Setup::Flavor';
+
 use strict;
 use warnings;
+
 our $VERSION = '0.01';
 
 1;
-__END__
 
-=head1 NAME
+=head1 TITLE
 
-Module::Setup::Flavor::JJNAPIORK -
+Module::Setup::Flavor::JJNAPIORK - How I use Module::Setup
 
 =head1 SYNOPSIS
 
-  use Module::Setup::Flavor::JJNAPIORK;
+  module-setup App::My::New::Project JJNAPIORK
 
 =head1 DESCRIPTION
 
-Module::Setup::Flavor::JJNAPIORK is
+This is a custom flavor for using L<Module::Setup> so that I can just build
+a default project skeleton that comes out reasonable close to the way I expect
+it to be.  That way I can avoid copying files around from an existing good
+module or running C<module-setup> and then having to delete and massage a lot
+of things.
 
 =head1 AUTHOR
 
-Default Name E<lt>default {at} example.comE<gt>
+John Napiorkowski C< <<jjnapiork@cpan.org>> >
 
-=head1 SEE ALSO
+=head1 COPYRIGHT & LICENSE
 
-=head1 LICENSE
+Copyright 2010, John Napiorkowski
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
 
 =cut
+
+__DATA__
+
+---
+file: Changes
+template: |
+  Revision history for Perl extension [% module %]
+  
+  0.01    [% localtime %]
+          - original version
+---
+file: Makefile.PL
+template: |
+  use inc::Module::Install;
+  name '[% dist %]';
+  all_from 'lib/[% module_unix_path %].pm';
+  
+  # requires '';
+  
+  tests 't/*.t';
+  author_tests 'xt';
+  
+  test_requires 'Test::More';
+  auto_set_repository;
+  auto_include;
+  WriteAll;
+---
+file: MANIFEST.SKIP
+template: |
+  \bRCS\b
+  \bCVS\b
+  ^MANIFEST\.
+  ^Makefile$
+  ~$
+  ^#
+  \.old$
+  ^blib/
+  ^pm_to_blib
+  ^MakeMaker-\d
+  \.gz$
+  \.cvsignore
+  ^t/9\d_.*\.t
+  ^t/perlcritic
+  ^tools/
+  \.svn/
+  ^[^/]+\.yaml$
+  ^[^/]+\.pl$
+  ^\.shipit$
+  ^\.git/
+  \.sw[po]$
+---
+file: README
+template: |
+  This is Perl module [% module %].
+  
+  INSTALLATION
+  
+  [% module %] installation is straightforward. If your CPAN shell is set up,
+  you should just be able to do
+  
+      % cpan [% module %]
+  
+  Download it, unpack it, then build it as per the usual:
+  
+      % perl Makefile.PL
+      % make && make test
+  
+  Then install it:
+  
+      % make install
+  
+  DOCUMENTATION
+  
+  [% module %] documentation is available as in POD. So you can do:
+  
+      % perldoc [% module %]
+  
+  to read the documentation online with your favorite pager.
+  
+  [% config.author %]
+---
+file: lib/____var-module_path-var____.pm
+template: |
+  package [% module %];
+  use strict;
+  use warnings;
+  our $VERSION = '0.01';
+  
+  1;
+  __END__
+  
+  =head1 NAME
+  
+  [% module %] -
+  
+  =head1 SYNOPSIS
+  
+    use [% module %];
+  
+  =head1 DESCRIPTION
+  
+  [% module %] is
+  
+  =head1 AUTHOR
+  
+  [% config.author %] E<lt>[% config.email %]E<gt>
+  
+  =head1 SEE ALSO
+  
+  =head1 LICENSE
+  
+  This library is free software; you can redistribute it and/or modify
+  it under the same terms as Perl itself.
+  
+  =cut
+---
+file: t/00_compile.t
+template: |
+  use strict;
+  use Test::More tests => 1;
+  
+  BEGIN { use_ok '[% module %]' }
+---
+file: xt/01_podspell.t
+template: |
+  use Test::More;
+  eval q{ use Test::Spelling };
+  plan skip_all => "Test::Spelling is not installed." if $@;
+  add_stopwords(map { split /[\s\:\-]/ } <DATA>);
+  $ENV{LANG} = 'C';
+  all_pod_files_spelling_ok('lib');
+  __DATA__
+  [% config.author %]
+  [% config.email %]
+  [% module %]
+---
+file: xt/02_perlcritic.t
+template: |
+  use strict;
+  use Test::More;
+  eval {
+      require Test::Perl::Critic;
+      Test::Perl::Critic->import( -profile => 'xt/perlcriticrc');
+  };
+  plan skip_all => "Test::Perl::Critic is not installed." if $@;
+  all_critic_ok('lib');
+---
+file: xt/03_pod.t
+template: |
+  use Test::More;
+  eval "use Test::Pod 1.00";
+  plan skip_all => "Test::Pod 1.00 required for testing POD" if $@;
+  all_pod_files_ok();
+---
+file: xt/perlcriticrc
+template: |
+  [TestingAndDebugging::ProhibitNoStrict]
+  allow=refs
+---
+config:
+  author: John Napiorkowski
+  class: Module::Setup::Flavor::Default
+  email: jjnapiork@cpan.org
+  plugins:
+    - Config::Basic
+    - Template
+    - Test::Makefile
+    - Additional
+    - VC::SVN
+    - VC::Git
+
+
+

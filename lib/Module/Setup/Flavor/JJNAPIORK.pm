86836e8fb840b9486c3d77531bf96c8da1765495
John Napiorkowski <jjn1056@yahoo.com>
2010-09-17 11:44:36 -0400

    first checkin

---
+++ lib/Module/Setup/Flavor/JJNAPIORK.pm
@@ -0,0 +1,32 @@
+package Module::Setup::Flavor::JJNAPIORK;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Setup::Flavor::JJNAPIORK -
+
+=head1 SYNOPSIS
+
+  use Module::Setup::Flavor::JJNAPIORK;
+
+=head1 DESCRIPTION
+
+Module::Setup::Flavor::JJNAPIORK is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
